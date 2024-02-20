6cda20e3cf42943bfdec4eee8b137132e4f7867f
natscott1738 <natebaraka777@gmail.com>
2024-02-19 22:06:27 -0800

    exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for element in my_list:
+            print(element, end = "")
+            count += 1
+            if count == x:
+                break
+            print()
+    except Exception:
+        pass
+    return count
