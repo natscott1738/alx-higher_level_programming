37de025d27449c07f70f1d145b27d609642e7080
natscott1738 <natebaraka777@gmail.com>
2024-03-28 12:13:56 -0700

    more queries

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,5 @@
 --Write a script that creates the table force_name on your MySQL server
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
