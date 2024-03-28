e20e7a1b2d1e332b21d3eeab91e3bb3ea9acd15b
natscott1738 <natebaraka777@gmail.com>
2024-03-28 11:57:29 -0700

    more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,8 @@
---Define MySQL user and password
-user="user_0d_1"
-password="user_0d_1_pwd"
+-- Create the user if it doesn't exist and set the password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
---Create MySQL user if it doesn't exist
-echo "CREATE USER IF NOT EXISTS '$user'@'localhost' IDENTIFIED BY '$password';" | mysql -hlocalhost -uroot -p
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
---Grant all privileges to the MySQL user
-echo "GRANT ALL PRIVILEGES ON *.* TO '$user'@'localhost' WITH GRANT OPTION;" | mysql -hlocalhost -uroot -p
+-- Apply the changes
+FLUSH PRIVILEGES;
