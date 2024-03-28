6236642a30bb0a6015d9d4dd5a35d6f4dbfb1c5f
natscott1738 <natebaraka777@gmail.com>
2024-03-28 11:47:57 -0700

    more queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,7 @@
 --lists all privileges of the MySQL users 
 CREATE USER 'user_0d_1'@'localhost';
 CREATE USER 'user_0d_2'@'localhost';
---grants privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
---shows privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
