7a6802ba133039cb4765b3de8a8ef393bca76618
natscott1738 <natebaraka777@gmail.com>
2024-03-28 11:31:16 -0700

    more queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,9 @@
 --lists all privileges of the MySQL users 
-mysql -hlocalhost -uroot -p -e "CREATE USER 'user_0d_1'@'localhost';"
-mysql -hlocalhost -uroot -p -e "CREATE USER 'user_0d_2'@'localhost';"
-mysql -hlocalhost -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';"
-mysql -hlocalhost -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';"
-echo "Grants for user_0d_1@localhost"
-mysql -hlocalhost -uroot -p -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
-echo "Grants for user_0d_2@localhost"
-mysql -hlocalhost -uroot -p -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+CREATE USER 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_2'@'localhost';
+--grants privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+--shows privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
