8614e4f77fc4cff5ee44627827fcbaf5e6effa48
natscott1738 <natebaraka777@gmail.com>
2024-05-10 03:19:05 -0700

    python networking

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# Display response body
+curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
